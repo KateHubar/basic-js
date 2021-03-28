fa51834c77e4e5745ed022dc2b16b00a0eca4fac
Kate Hubar <kate.hubar@gmail.com>
2021-03-28 20:50:48 +0300

    feat: add dreamteam implementation

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!members || !(members instanceof Array)) return false;
+    return members
+        .filter((el) => typeof el === "string")
+        .map((str) => {
+            str = str.trim();
+            return str[0].toUpperCase();
+        })
+        .sort()
+        .join("");
 };
