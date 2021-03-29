fd734090342ba0a2e797f9f533231fe3fd9328c0
Kate Hubar <kate.hubar@gmail.com>
2021-03-29 18:29:27 +0300

    feat: add transform array task implementation

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!arr || !(arr instanceof Array)) throw Error("Error");
+    if (!arr.length) return [];
+    const editableArray = [...arr];
+    const result = [];
+    let discard = false;
+
+    for (let index = 0; index < editableArray.length; index++) {
+        if (discard) {
+            discard = false;
+            continue;
+        }
+        if (typeof editableArray[index] === "string") {
+            const control = editableArray[index];
+            switch (control) {
+                case "--discard-next":
+                    editableArray[index + 1] = undefined;
+                    discard = true;
+                    break;
+                case "--discard-prev":
+                    if (result.length && editableArray[index - 1] !== undefined)
+                        result.length--;
+                    break;
+                case "--double-next":
+                    if (editableArray[index + 1] !== undefined)
+                        result.push(editableArray[index + 1]);
+                    break;
+                case "--double-prev":
+                    if (result.length && editableArray[index - 1] !== undefined)
+                        result.push(editableArray[index - 1]);
+                    break;
+                default:
+                    result.push(control);
+                    break;
+            }
+        } else {
+            result.push(editableArray[index]);
+        }
+    }
+    return result;
 };
